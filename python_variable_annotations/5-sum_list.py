5223a184e785fb515624dad0c47e8d25bd816b27
MalachisProductions02 <malachisproductionsnooficial@gmail.com>
2025-07-28 16:47:17 +0000

    tasko5_corrections01

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """Function that sums a list of floats."""
 
-
 from typing import List
+
 
 def sum_list(input_list: List[float]) -> float:
     """Returns the sum of a list of floats."""
