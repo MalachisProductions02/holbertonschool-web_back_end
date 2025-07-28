071bd4d6983c04937ec2acee860dff22cb6b7be5
MalachisProductions02 <malachisproductionsnooficial@gmail.com>
2025-07-28 17:46:35 +0000

    task01

---
+++ python_async_comprehension/1-async_comprehention.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Async comprehension collecting 10 random numbers from async_generator."""
+
+from typing import List
+from 0_async_generator import async_generator
+
+async def async_comprehension() -> List[float]:
+    """
+    Coroutine that collects 10 random numbers using async comprehension
+    over async_generator and returns them as a list.
+    """
+    return [i async for i in async_generator()]
