3d2aaff75fffbbbbc2d0357e8139094f649e0587
MalachisProductions02 <malachisproductionsnooficial@gmail.com>
2025-07-28 17:11:49 +0000

    task08_corrections04

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """
-This module contains the function make_multiplier which returns 
-a function that multiplies a float by a given multiplier.
+This module contains the function make_multiplier 
+which returns a function that multiplies a 
+float by a given multiplier.
 """
 
 
@@ -10,7 +11,8 @@ from typing import Callable
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
     """
-    Returns a function that multiplies its float input by the provided multiplier.
+    Returns a function that multiplies 
+    its float input by the provided multiplier.
     """
     return lambda x: x * multiplier
 
