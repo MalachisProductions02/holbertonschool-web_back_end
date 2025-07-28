eb884b722a86c9320b016067cc894837895ef429
MalachisProductions02 <malachisproductionsnooficial@gmail.com>
2025-07-28 17:04:43 +0000

    task08

---
+++ python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+This module contains the function make_multiplier
+which returns a function that multiplies a float by a given multiplier.
+"""
+
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """
+    Returns a function that multiplies its float input by the provided multiplier.
+    """
+    return lambda x: x * multiplier
