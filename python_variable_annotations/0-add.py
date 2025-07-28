e7254dd5f60b78ea83ba4e4cd5d31499dae6fc85
MalachisProductions02 <malachisproductionsnooficial@gmail.com>
2025-07-28 16:27:20 +0000

    task00_corrections02

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
-
+"""Module that provides a type-annotated add function.
+"""
 def add(a: float, b: float) -> float:
+    """Adds two float numbers and returns the result as a float."""
     return a + b
