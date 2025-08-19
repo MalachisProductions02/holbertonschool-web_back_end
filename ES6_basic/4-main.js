ba920bf5724f16643a8483e17d6bfb928735b1f7
MalachisProductions02 <malachisprodutuctionsnooficial@gmail.com>
2025-08-19 00:36:37 +0000

    task04

---
+++ ES6_basic/4-main.js
@@ -0,0 +1,5 @@
+// 4-main.js
+import returnHowManyArguments from './4-rest-parameter.js';
+
+console.log(returnHowManyArguments("one"));              // 1
+console.log(returnHowManyArguments("one", "two", 3, "4th")); // 4
