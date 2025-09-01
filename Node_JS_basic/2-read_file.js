549738cf2334dc2638a1ef04ffd383bb8bbad385
MalachisProductions02 <malachisproductionsnooficial@gmail.com>
2025-09-01 16:27:20 +0000

    task02

---
+++ Node_JS_basic/2-read_file.js
@@ -0,0 +1,36 @@
+// 2-read_file.js
+const fs = require('fs');
+
+function countStudents(path) {
+  try {
+    const data = fs.readFileSync(path, 'utf-8').trim();
+    const lines = data.split('\n').filter((line) => line.trim() !== '');
+
+    // quitamos encabezado
+    const students = lines.slice(1);
+
+    console.log(`Number of students: ${students.length}`);
+
+    const fields = {};
+    for (const line of students) {
+      const parts = line.split(',');
+      const firstname = parts[0];
+      const field = parts[3];
+
+      if (!fields[field]) {
+        fields[field] = [];
+      }
+      fields[field].push(firstname);
+    }
+
+    for (const [field, list] of Object.entries(fields)) {
+      console.log(
+        `Number of students in ${field}: ${list.length}. List: ${list.join(', ')}`
+      );
+    }
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
+}
+
+module.exports = countStudents;
